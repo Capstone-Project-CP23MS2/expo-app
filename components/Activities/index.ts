f6f8316aa0c36c004bfab365319d9029291220cb
Purachet Zheng <poorachad2545@gmail.com>
2023-12-11 23:17:11 +0700

    test

--- components/Activities/index.ts
+++ components/Activities/index.ts
@@ -1,4 +1,4 @@
-import { default as ActivityCard } from './components/Card';
+import { default as ActivityCard } from './Card';
 import { default as ActivityList } from './components/List';
 
 import { default as ActivityDetailFooter } from './Detail/components/Footer';
