a81357729585436a935772ff24d36a055c69c40a
Purachet Zheng <poorachad2545@gmail.com>
2024-03-25 04:24:32 +0700

    refactor: removed old auth context

--- modules/auth/components/protectedLayout.tsx
+++ modules/auth/components/protectedLayout.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import { useRouter } from 'expo-router'
-import { useAuth } from '@/context/auth_old'
+import { useAuth } from '@/context/authContext'
 
 export default function protectedLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter()
