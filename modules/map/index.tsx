2c53c83ee4f943614a16f249003e434495340373
Phumin63130500094 <chumphu.phumin@gmail.com>
2024-03-29 16:44:15 +0700

    update: update map marker

--- modules/map/index.tsx
+++ modules/map/index.tsx
@@ -2,13 +2,25 @@ import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 
 import { COLORS, SIZES } from '@/constants'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import { FontAwesome5 } from '@expo/vector-icons'
+import { Stack } from 'expo-router'
 
 const MapActivities = () => {
   return (
     <View style={{ flex: 1 }}>
-      <MapView style={styles.map} provider={PROVIDER_GOOGLE} />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }} />
+      </MapView>
     </View>
   )
 }
