731d7afe058e6bd86230aee5eec6afbc1da44751
Purachet Zheng <poorachad2545@gmail.com>
2024-03-30 04:31:00 +0700

    feat: added setting list to profile screen

--- modules/profile/index.tsx
+++ modules/profile/index.tsx
@@ -2,17 +2,50 @@ import { useAuth } from '@/context/authContext'
 import { UseDeleteUser } from '@/hooks/useAPI'
 import { useRouter } from 'expo-router'
 import React, { useState } from 'react'
-import { SafeAreaView, Modal, StyleSheet, View } from 'react-native'
-import { Button, Text } from 'react-native-ui-lib'
+import { SafeAreaView, Modal, StyleSheet, View, Pressable } from 'react-native'
+import { Button, ListItem, Text } from 'react-native-ui-lib'
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons'
 import { SIZES } from '@/constants'
+import { createStyleSheet, useStyles } from 'react-native-unistyles'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const { styles } = useStyles(stylesheet)
   const { user, onLogout } = useAuth()
   const router = useRouter()
   const deleteMutation = UseDeleteUser()
+
+  const settingList = [
+    {
+      id: 'user-edit',
+      title: 'แก้ไขข้อมูล',
+      icon: 'edit',
+      onPress: () => {
+        console.log('👤 Edit Profile')
+
+        router.push('/profile/edit')
+      },
+    },
+    {
+      id: 'user-interest',
+      title: 'สิ่งที่สนใจ',
+      icon: 'heart',
+      onPress: () => {
+        console.log('👤 Edit Interest')
+      },
+    },
+    {
+      id: 'user-manage-account',
+      title: 'จัดการบัญชี',
+      icon: 'account',
+      onPress: () => {
+        console.log('👤 ManageAccount')
+        router.push('/profile/manage-account')
+      },
+    },
+  ]
 
   return (
     <SafeAreaView style={styles.container}>
@@ -22,18 +55,54 @@ const Page = (props: Props) => {
           <Text style={{ fontWeight: 'bold', fontSize: SIZES.large }}>{user?.username}</Text>
           <Text md>{user?.email}</Text>
         </View>
+
+        <View style={styles.settingList}>
+          {settingList.map((item, index) => (
+            <Pressable
+              key={item.id}
+              style={styles.settingItemWrapper}
+              activeOpacity={0.5}
+              onPress={item.onPress}
+              android_ripple={{ color: 'gray' }}
+            >
+              <View style={styles.settingItem}>
+                <Text style={styles.settingTitle}>{item.title}</Text>
+                <MaterialIcons name="chevron-right" size={24} color="black" />
+              </View>
+            </Pressable>
+          ))}
+        </View>
         <Button label="Sign Out" onPress={onLogout} />
       </View>
     </SafeAreaView>
   )
 }
-const styles = StyleSheet.create({
+
+const stylesheet = createStyleSheet(theme => ({
   container: {
     flex: 1,
     backgroundColor: 'white',
   },
+  settingList: {
+    flexDirection: 'column',
+  },
+  settingItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: theme.spacings.lg,
+    borderBottomColor: '#DCDCDC',
+    borderBottomWidth: 1,
+  },
+  settingItemWrapper: {
+    paddingHorizontal: theme.spacings.lg,
+  },
+  settingTitle: {
+    ...theme.typography.md,
+  },
+
   content: {
-    padding: 20,
+    // padding: 20,
     gap: 5,
   },
   header: {
@@ -60,6 +129,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingLeft: 15,
   },
-})
+}))
 
 export default Page
